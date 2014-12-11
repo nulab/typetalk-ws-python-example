dcba5c59f606665bbe8290c19ad9e4758c1c6605
someda <someda@isenshi.com>
2014-12-11 12:25:10 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## Typetalk WebSocket client sample
+
+### Setup
+
+#### Install required libraries
+
+```
+$ pip install -r requirements.txt
+```
+
+#### Register an application
+
+[Create your application](https://typetalk.in/my/develop/applications/register) to use API. When issuing client id and client secret, you should select "Client Credentials" for "Grant Type".
+
+### Run sample application
+
+You should set the following environment variables to run the sample application in advance.
+
+```
+$ export TYPETALK_CLIENT_ID=XXXX
+$ export TYPETALK_CLIENT_SECRET=XXXX
+$ ./main.py
+```
---
+++ main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import urllib
+import urllib2
+import json
+
+import websocket
+
+
+def issue_access_token(client_id, client_secret):
+    params = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials',
+    }
+    data = urllib.urlencode(params)
+    res = urllib2.urlopen(urllib2.Request('https://typetalk.in/oauth2/access_token', data))
+    return json.load(res)['access_token']
+
+
+def on_message(ws, message):
+    print message
+
+
+def on_error(ws, error):
+    print error
+
+
+def on_close(ws):
+    print 'disconnected streaming server'
+
+
+def on_open(ws):
+    print 'connected streaming server'
+
+
+def main():
+    if not ('TYPETALK_CLIENT_ID' in os.environ and 'TYPETALK_CLIENT_SECRET' in os.environ):
+        print 'TYPETALK_CLIENT_ID and TYPETALK_CLIENT_SECRET should be set in your environment variables'
+        sys.exit(1)
+
+    access_token = issue_access_token(os.environ['TYPETALK_CLIENT_ID'], os.environ['TYPETALK_CLIENT_SECRET'])
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("wss://typetalk.in/api/v1/streaming",
+                                header=["Authorization: Bearer %s" % access_token],
+                                on_open=on_open,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    try:
+        ws.run_forever()
+    except KeyboardInterrupt:
+        ws.close()
+
+
+if __name__ == "__main__":
+    main()
